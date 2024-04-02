64c2920972b7fbf3b7d080cd53e56f8aecb54b0a
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-04-02 20:55:58 -0300

    using make with maps

--- main.go
+++ main.go
@@ -6,9 +6,16 @@ func main() {
 	userNames := make([]string, 2, 3)
 	userNames[0] = "Julia"
 	userNames[1] = "Mary"
-	userNames = append(userNames, "Max")
-	userNames = append(userNames, "Leo")
-	userNames = append(userNames, "Manuel")
-	userNames = append(userNames, "Veronica")
-	fmt.Println(userNames)
+	// userNames = append(userNames, "Max")
+	// userNames = append(userNames, "Leo")
+	// userNames = append(userNames, "Manuel")
+	// userNames = append(userNames, "Veronica")
+	//fmt.Println(userNames)
+
+	courseRating := make(map[string]float64, 3)
+	courseRating["Go"] = 4.7
+	courseRating["React"] = 4.8
+	courseRating["Angular"] = 5.2
+
+	fmt.Println(courseRating)
 }
